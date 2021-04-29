a37247a75a61a320ca1642e89e575b51bef531ca
Joseph Davis <joethebartenderpdx@gmail.com>
2021-04-29 11:11:52 -0700

    search almost working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PokeList from './PokeList';
 import Footer from './Footer';
 import PokeSearch from './PokeSearch';
 
-const POKEDEX_API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+const POKEDEX_API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex?page=3&perPage=50';
 class App extends Component {
 
   state = {
@@ -17,7 +17,22 @@ class App extends Component {
   async componentDidMount() {
     const response = await request.get(POKEDEX_API_URL);
     this.setState({ pokemon: response.body.results });
-    console.log(response.body.results);
+  }
+
+  handleSearch = ({ searchField }) => {
+    const nameRegex = new RegExp(searchField, 'i');
+    const { pokemon } = this.state;
+
+    const searchedPoke = pokemon
+      .filter(poke => {
+        return !searchField || poke.pokemon.match(nameRegex);
+      })
+      .sort((a, b) => {
+        if (a[searchField] < b[searchField]) return -1;
+        if (a[searchField] < b[searchField]) return 1;
+        return 0;
+      });
+    this.setState({ pokemon: searchedPoke });
   }
 
   render() {
@@ -26,12 +41,12 @@ class App extends Component {
       <div className="App">
       
         <Header/>
-        <main>
 
-          <PokeSearch/>
+        <section>
+          <PokeSearch onSearch={ this.handleSearch }/>
           <PokeList pokemon={pokemon}/>
+        </section>
 
-        </main>
         <Footer/>
 
       </div>
--- src/PokeSearch.js
+++ src/PokeSearch.js
@@ -2,23 +2,39 @@ import React, { Component } from 'react';
 import './PokeSearch.css';
 
 export default class PokeSearch extends Component {
-  state = { searchField: '', sortField: '' }
+  state = { searchField: '' }
   
+  handleSearchField = ({ target }) => {
+    this.setState({ searchField: target.value });
+  }
+
+  componentDidMount() {
+    const { searchField } = this.props;
+    this.setState({ searchField: searchField });
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.onSearch(this.state);
   }
 
+
   render() {
+    const { searchField, sortField } = this.state;
+
     return (
       <form className="PokeSearch" onSubmit={this.handleSubmit}>
 
-        <input className='searchField'></input>
+        <input className='searchField'
+          value={searchField}
+          onChange={ this.handleSearchField }></input>
 
-        <select name='sortField'>
+        <select name='sortField'
+          value={sortField}
+          onChange={ this.handleSortField }>
           <option value=''>Sort By...</option>
           <option value='Name'>Name</option>
-          <option value='Type'>Type</option>
+          <option value='HP'>HP</option>
         </select>
 
         <button>Search</button>
