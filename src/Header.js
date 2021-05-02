f9290bcdb2f444c79b0bc34b64d7dc54154ba81d
Joseph Davis <joethebartenderpdx@gmail.com>
2021-05-02 16:06:45 -0700

    cool marquee addition i stumbled across

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { motion } from 'framer-motion';
 import './Header.css';
 
+const variants = {
+  animate: {
+    x: [180, -1435],
+    transition: {
+      x: {
+        repeat: Infinity,
+        repeatType: 'loop',
+        duration: 10,
+        ease: 'linear',
+      },
+    },
+  },
+};
 export default class Header extends Component {
-  
   render() {
     return (
+
       <header className="Header">
-        <h1>Joe's Incredibly Bland Pokedex</h1>
+        <motion.div
+          className='track'
+          variants={variants}
+          animate='animate'
+        >
+          <h1>Joe's Incredibly Bland Pokedex</h1>
+
+        </motion.div>
       </header>
+
+
     );
   }
 
